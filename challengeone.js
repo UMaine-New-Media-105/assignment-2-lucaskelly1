d5dd953ff2c9b6d6929b6e51e7b463cc8553526b
lucaskelly1 <124173836+lucaskelly1@users.noreply.github.com>
2023-02-15 16:15:30 -0500

    Create challengeone.js
    My code for the L in my first name Lucas - using function for column.

---
+++ challengeone.js
@@ -0,0 +1,59 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("black");
+  
+  drawcolumnone(0,100,1)
+  drawcolumnone(0+80,100,1)
+  drawcolumnone(0+160,100,1)
+  drawcolumnone(0+240,100,1)
+
+
+  drawothercolumn(0+80,100,1)
+  drawothercolumn(0+160,100,1)
+  drawothercolumn(0+240,100,1)
+  drawothercolumn(0+320,100,1)
+
+}
+//column 2-4 for blue in space
+
+function drawothercolumn(x,y,size,ifColor) {
+  
+
+  scale(size)
+fill("blue")
+  
+//column 2-4 for blue
+  rect(x,0,80,80);
+  rect(x,80,80,80);
+  rect(x,160,80,80);
+  rect(x,240,80,80);
+  rect(x+240,320,80,80);
+
+
+  endShape(CLOSE);
+  
+  
+  
+}
+
+function drawcolumnone(x,y,size,ifColor) {
+  
+
+  scale(size)
+fill("red")
+  
+//column 1 for red
+  rect(x,0,80,80);
+  rect(x,80,80,80);
+  rect(x,160,80,80);
+  rect(x,240,80,80);
+  rect(x,320,80,80);
+
+  endShape(CLOSE);
+  
+  
+  
+}
